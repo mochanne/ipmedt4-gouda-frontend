66f1e2692f2886c20f590070b2bccc4c5ba5ae3b
evavandongen <96053258+evavandongen@users.noreply.github.com>
2022-06-14 10:25:36 +0200

    routing setup
    / => homepage
    /map => mapview

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import MapView from './components/MapView';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Home from './pages/Home';
+import MapView from './pages/MapView';
+
 function App() {
   return (
-    <div className="App">
-      <MapView/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/map' element={ <MapView /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
